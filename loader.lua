fa057a42e5b34cd3249f966b81cc24dfe598eb3e
dragonstrukov <158556828+dragonstrukov@users.noreply.github.com>
2026-07-14 14:13:43 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,11 @@
-print("soon")
+if game.PlaceId == 537413528 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/dragonstrukov/MacrOS/refs/heads/main/Build-A-Boat-For-Treasure/script.lua"))()
+elseif game.PlaceId == 1930863474 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/dragonstrukov/MacrOS/refs/heads/main/Build-A-Boat-For-Treasure/Inner-Cloud/script.lua"))()
+elseif game.PlaceId == 1930866268 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/dragonstrukov/MacrOS/refs/heads/main/Build-A-Boat-For-Treasure/Christmas/script.lua"))()
+elseif game.PlaceId == 1930665568 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/dragonstrukov/MacrOS/refs/heads/main/Build-A-Boat-For-Treasure/Halloween/script.lua"))()
+elseif game.PlaceId == 5178049460 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/dragonstrukov/MacrOS/refs/heads/main/Build-A-Boat-For-Treasure/Project-Zeg/script.lua"))()
+end

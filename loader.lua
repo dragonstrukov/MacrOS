9e601f4cb005927c7ca8dc1a39aa8625221057ac
dragonstrukov <158556828+dragonstrukov@users.noreply.github.com>
2026-09-10 14:02:42 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -12,6 +12,8 @@ elseif game.PlaceId == 142823291 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/dragonstrukov/MacrOS/refs/heads/main/Murder-Mystery-2/script.lua"))()
 elseif game.PlaceId == 7009987220 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/dragonstrukov/MacrOS/refs/heads/main/Ragdoll-Stack/script.lua"))()
+elseif game.PlaceId == 72659788689464 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/dragonstrukov/MacrOS/refs/heads/main/Life-in-Prison/script.lua"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/dragonstrukov/MacrOS/refs/heads/main/Untitled/script.lua"))()
 end
